f159e9dc6b9b3b01d30b42503191dd17d21c208a
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-03-05 18:55:26 +0100

    Add function to uncover environment changes

--- diff.go
+++ diff.go
@@ -19,6 +19,15 @@ type Var struct {
 // Env represents an environment, a list of environment variables.
 type Env []Var
 
+// asMap return the environment e as a map.
+func (e Env) asMap() map[string][]string {
+	m := make(map[string][]string)
+	for _, v := range e {
+		m[v.Name] = v.Value
+	}
+	return m
+}
+
 // Parse parses the string s with environment variables as printed by `env` or
 // `printenv` and returns an Env.
 func Parse(s string) Env {
@@ -35,4 +44,62 @@ func Parse(s string) Env {
 		env = append(env, Var{name, strings.Split(value, pathListSeparator)})
 	}
 	return env
+}
+
+// Diff returns an Env with environment variables that were unset from old, and
+// set or overwritten in new. The result is such that applying the diff to an
+// old environment turns it into new.
+func Diff(old, new Env) Env {
+	var diff Env
+	oldMap := old.asMap()
+	newMap := new.asMap()
+	// Look for variables that were unset.
+	for _, v := range old {
+		if _, ok := newMap[v.Name]; ok {
+			continue
+		}
+		diff = append(diff, Var{Name: v.Name})
+	}
+	var (
+		val []string
+		ok  bool
+	)
+	// Look for variables that were kept or overwritten.
+	for _, v := range new {
+		// Ignore variables that are in a and b with the same value.
+		if val, ok = oldMap[v.Name]; ok && stringSliceEqual(v.Value, val) {
+			continue
+		}
+		// Look for common prefix or suffix.
+		for i := 0; i < len(v.Value) && i < len(val); i++ {
+			// Common suffix: PATH=/new/path:$PATH.
+			if stringSliceEqual(v.Value[i+1:], val) {
+				v.Value = append(v.Value[:i+1], "$"+v.Name)
+				break
+			}
+			// Common prefix: PATH=$PATH:/new/path.
+			if stringSliceEqual(v.Value[:i+1], val) {
+				v.Value = append([]string{"$" + v.Name}, v.Value[i+1:]...)
+				break
+			}
+		}
+		diff = append(diff, v)
+	}
+	return diff
+}
+
+// stringSliceEqual returns true if s and t are equal.
+func stringSliceEqual(s, t []string) bool {
+	if (s == nil) != (t == nil) {
+		return false
+	}
+	if len(s) != len(t) {
+		return false
+	}
+	for i := range s {
+		if s[i] != t[i] {
+			return false
+		}
+	}
+	return true
 }
--- diff_test.go
+++ diff_test.go
@@ -62,3 +62,140 @@ func TestParse(t *testing.T) {
 		}
 	}
 }
+
+var diffTests = []struct {
+	desc     string
+	old, new Env
+	diff     Env
+}{
+	{},
+	{
+		desc: "set variable",
+		old:  nil,
+		new:  Env{{"TERM", []string{"xterm"}}},
+		diff: Env{{"TERM", []string{"xterm"}}},
+	},
+	{
+		desc: "unset variable",
+		old:  Env{{"TERM", []string{"xterm"}}},
+		new:  nil,
+		diff: Env{{"TERM", nil}},
+	},
+	{
+		desc: "unset and set different variables",
+		old:  Env{{"TERM", []string{"xterm"}}},
+		new:  Env{{"PWD", []string{"/"}}},
+		diff: Env{
+			{"TERM", nil},
+			{"PWD", []string{"/"}},
+		},
+	},
+	{
+		desc: "unset, set and keep existing (sorted)",
+		old: Env{
+			{"HOME", []string{"/root"}},
+			{"TERM", []string{"xterm"}},
+		},
+		new: Env{
+			{"HOME", []string{"/root"}},
+			{"PWD", []string{"/"}},
+		},
+		diff: Env{
+			{"TERM", nil},
+			{"PWD", []string{"/"}},
+		},
+	},
+	{
+		desc: "unset, set and keep existing (unsorted)",
+		old: Env{
+			{"TERM", []string{"xterm"}},
+			{"HOME", []string{"/root"}},
+		},
+		new: Env{
+			{"HOME", []string{"/root"}},
+			{"PWD", []string{"/"}},
+		},
+		diff: Env{
+			{"TERM", nil},
+			{"PWD", []string{"/"}},
+		},
+	},
+	{
+		desc: "unset, overwrite and set",
+		old: Env{
+			{"TERM", []string{"xterm"}},
+			{"HOME", []string{"/root"}},
+		},
+		new: Env{
+			{"HOME", []string{"/home/user"}},
+			{"PWD", []string{"/"}},
+		},
+		diff: Env{
+			{"TERM", nil},
+			{"HOME", []string{"/home/user"}},
+			{"PWD", []string{"/"}},
+		},
+	},
+	{
+		desc: "unset, overwrite and keep existing (including empty)",
+		old: Env{
+			{"TERM", []string{"xterm"}},
+			{"HOME", []string{"/root"}},
+		},
+		new: Env{
+			{"HOME", []string{"/home/user"}},
+			{"PWD", []string{"/"}},
+			{"FOO", []string{}},
+		},
+		diff: Env{
+			{"TERM", nil},
+			{"HOME", []string{"/home/user"}},
+			{"PWD", []string{"/"}},
+			{"FOO", []string{}},
+		},
+	},
+	{
+		desc: "prepend value",
+		old:  Env{{"PATH", []string{"/bin"}}},
+		new:  Env{{"PATH", []string{"/sbin", "/bin"}}},
+		diff: Env{{"PATH", []string{"/sbin", "$PATH"}}},
+	},
+	{
+		desc: "prepend values",
+		old:  Env{{"PATH", []string{"/sbin", "/bin"}}},
+		new:  Env{{"PATH", []string{"/usr/sbin", "/usr/bin", "/sbin", "/bin"}}},
+		diff: Env{{"PATH", []string{"/usr/sbin", "/usr/bin", "$PATH"}}},
+	},
+	{
+		desc: "append value",
+		old:  Env{{"PATH", []string{"/sbin"}}},
+		new:  Env{{"PATH", []string{"/sbin", "/bin"}}},
+		diff: Env{{"PATH", []string{"$PATH", "/bin"}}},
+	},
+	{
+		desc: "append values",
+		old:  Env{{"PATH", []string{"/usr/sbin", "/usr/bin"}}},
+		new:  Env{{"PATH", []string{"/usr/sbin", "/usr/bin", "/sbin", "/bin"}}},
+		diff: Env{{"PATH", []string{"$PATH", "/sbin", "/bin"}}},
+	},
+}
+
+func TestDiff(t *testing.T) {
+	for _, tt := range diffTests {
+		if got := Diff(tt.old, tt.new); !envEqual(got, tt.diff) {
+			t.Errorf("%s:\nDiff(%v, %v) = %v, want %v", tt.desc, tt.old, tt.new, got, tt.diff)
+		}
+	}
+}
+
+func envEqual(a, b Env) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i].Name != b[i].Name || !stringSliceEqual(a[i].Value, b[i].Value) {
+			return false
+		}
+	}
+	return true
+}
