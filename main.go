7a6c57ce8ab6076c4c6d8ba8430051450a4e53b4
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-03-29 18:59:38 +0200

    Support reverse-engineering SCL collection

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
+
+var output = flag.String("o", "", "set to 'docker' to print an ENV Dockerfile instruction")
 
 func main() {
 	flag.Parse()
+	if flag.Arg(0) == "scl" {
+		image := flag.Arg(1)
+		if err := writeInstalledCollectionsEnvDiff(os.Stdout, image, *output == "docker"); err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR:", err)
+		}
+		return
+	}
 	old := flag.Arg(0)
 	new := flag.Arg(1)
 	fmt.Println(Diff(Parse(old), Parse(new)))
